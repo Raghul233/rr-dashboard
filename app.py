11a17828752278a0ef391d588d0875c3251d9d7a
Raghul M <raghul@everstage.com>
2026-07-02 18:47:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1632,7 +1632,7 @@ with tab4:
         )
         
         common_padding = {
-            "left": 50,
+            "left": 45,
             "right": 10,
             "top": 5,
             "bottom": 40,
@@ -1741,10 +1741,10 @@ with tab4:
                     width=chart_width,
                     height=chart_height,
                     padding={
-                        "left": 50,
+                        "left": 45,
                         "right": 10,
                         "top": 5,
-                        "bottom": 45,
+                        "bottom": 40,
                     },
                 )
             )
